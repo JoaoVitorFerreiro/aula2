e85bbe1b12252abc68347631746805c064effb8c
JoãoVitorFerreiro <jvferreiro1@gmail.com>
2024-02-05 21:54:01 -0300

    Implantação

--- aula_2/src/aula_2/Cachorro.java
+++ aula_2/src/aula_2/Cachorro.java
@@ -6,6 +6,38 @@ public class Cachorro {
 	private int idade;
 	private String cor;
 	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	
+	public void setIdade (int idade) {
+		this.idade = idade;
+	}
+	
+	public void setCor (String cor) {
+		this.cor = cor;
+	}
+	
 	
 	public void Latir() {
 		System.out.println("O cachorro: " 
